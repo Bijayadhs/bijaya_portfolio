7ee6fa29f2b17665160a6cd23d09f76dff5590b7
Bijayadhs <bijayadhikari107@gmail.com>
2021-09-12 11:08:23 +1000

    Contact Me section done

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: kPrimaryColor,
+        primaryColor: kSecondaryColor,
       ),
       home: HomeScreen(),
     );
--- lib/pages/contact_me_screen.dart
+++ lib/pages/contact_me_screen.dart
@@ -12,35 +12,122 @@ class ContactMeScreen extends StatelessWidget {
         color: Color(0xFFEDF2F8),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(40.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            TitleWidget(text1: "Let's", text2: "Talk"),
+            SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  "Let's",
-                  style: kHighlightTitle,
+                ContactWidget(
+                  icon: Icons.mail_outline,
+                  text: ' bijayadhikari107@gmail.com',
                 ),
-                SizedBox(width: 4),
-                Text(
-                  "Talk",
-                  style: kNormalTitle,
-                ),
+                SizedBox(width: 20),
+                ContactWidget(
+                    icon: Icons.phone_enabled_outlined, text: ' +61424428959'),
               ],
             ),
+            SizedBox(height: 40),
+            TitleWidget(text1: "Find", text2: "Me"),
+            SizedBox(height: 20),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('Email'),
-                Text('Phone'),
+                SocialWidget(icon: Icons.link),
+                SizedBox(width: 20),
+                SocialWidget(icon: Icons.link),
+                SizedBox(width: 20),
+                SocialWidget(icon: Icons.link),
               ],
-            )
+            ),
+            SizedBox(height: 40),
+            CopyrightWidget(),
           ],
         ),
       ),
     );
+  }
+}
+
+class CopyrightWidget extends StatelessWidget {
+  const CopyrightWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('copyright © 2021 Bijaya Adhikari, All right reserved. ');
+  }
+}
+
+class SocialWidget extends StatelessWidget {
+  final IconData icon;
+  const SocialWidget({
+    Key? key,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      child: Icon(icon),
+      backgroundColor: Colors.white,
+    );
+  }
+}
+
+class TitleWidget extends StatelessWidget {
+  final String text1;
+  final String text2;
+  const TitleWidget({
+    Key? key,
+    required this.text1,
+    required this.text2,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          text1,
+          style: kHighlightTitle,
+        ),
+        SizedBox(width: 4),
+        Text(
+          text2,
+          style: kNormalTitle,
+        ),
+      ],
+    );
+  }
+}
+
+class ContactWidget extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  const ContactWidget({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+        ),
+        child: Row(
+          children: [
+            Icon(icon),
+            Text(text),
+          ],
+        ));
   }
 }
